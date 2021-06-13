1bfa820d95ac074e70ffc711f452df7151eb637a
dasaldan <85257902+dasaldan@users.noreply.github.com>
2021-06-13 13:53:17 +0100

    practical 3/4/5
    practical 3/4/5

--- Week3/exercise_script.py
+++ Week3/exercise_script.py
@@ -19,40 +19,50 @@ plt.ion()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # in this section, write the script to load the data and complete the main part of the analysis.
+counties = gpd.read_file('data_files/Counties.shp')
+NI_Wards = gpd.read_file('data_files/NI_Wards.shp')
 # try to print the results to the screen using the format method demonstrated in the workbook
+counties = counties.to_crs(epsg=32629)
+NI_Wards = NI_Wards.to_crs(epsg=32629)
 
+for col_counties in counties.columns:
+    print(col_counties)
+
+for col_NI_Wards in NI_Wards.columns:
+    print(col_NI_Wards)
 # load the necessary data here and transform to a UTM projection
 
 # your analysis goes here...
-
+join = gpd.sjoin(counties, NI_Wards, how='inner', lsuffix='left', rsuffix='right')
+print(join)
 # ---------------------------------------------------------------------------------------------------------------------
 # below here, you may need to modify the script somewhat to create your map.
 # create a crs using ccrs.UTM() that corresponds to our CRS
-myCRS = ccrs.UTM(29)
+### myCRS = ccrs.UTM(29)
 # create a figure of size 10x10 (representing the page size in inches
-fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
+### fig, ax = plt.subplots(1, 1, figsize=(10, 10), subplot_kw=dict(projection=myCRS))
 
 # add gridlines below
-gridlines = ax.gridlines(draw_labels=True,
-                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
-                         ylocs=[54, 54.5, 55, 55.5])
-gridlines.right_labels = False
-gridlines.bottom_labels = False
+### gridlines = ax.gridlines(draw_labels=True,
+###                         xlocs=[-8, -7.5, -7, -6.5, -6, -5.5],
+###                         ylocs=[54, 54.5, 55, 55.5])
+### gridlines.right_labels = False
+### gridlines.bottom_labels = False
 
 # to make a nice colorbar that stays in line with our map, use these lines:
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
+### divider = make_axes_locatable(ax)
+### cax = divider.append_axes("right", size="5%", pad=0.1, axes_class=plt.Axes)
 
 # plot the ward data into our axis, using
-ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
-                       legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
+### ward_plot = wards.plot(column='Population', ax=ax, vmin=1000, vmax=8000, cmap='viridis',
+###                       legend=True, cax=cax, legend_kwds={'label': 'Resident Population'})
 
-county_outlines = ShapelyFeature(counties['geometry'], myCRS, edgecolor='r', facecolor='none')
+### county_outlines = ShapelyFeature(counties['geometry'], myCRS, edgecolor='r', facecolor='none')
 
-ax.add_feature(county_outlines)
-county_handles = generate_handles([''], ['none'], edge='r')
+### ax.add_feature(county_outlines)
+### county_handles = generate_handles([''], ['none'], edge='r')
 
-ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
+### ax.legend(county_handles, ['County Boundaries'], fontsize=12, loc='upper left', framealpha=1)
 
 # save the figure
 # fig.savefig('sample_map.png', dpi=300, bbox_inches='tight')
